f88e2f9a990466918116ea97baedf57381009243
René Mäkeler <maekeler@marmalade.de>
2020-03-19 16:41:54 +0100

    Cleanup unused components

--- patterns/ProductList/index.js
+++ patterns/ProductList/index.js
@@ -1,5 +1,4 @@
 import ProductTile from './ProductTile'
-import { Heading, Copytext, Button } from '..'
 
 function ProductList(props) {
   const { products = [] } = props
@@ -10,24 +9,6 @@ function ProductList(props) {
         <ProductTile key={product.id} {...product.fields} />
       ))}
     </section>
-  )
-}
-
-// TODO: Remove hard-coded implementation
-function ProductPriceHint(props) {
-  return (
-    <div className="product-item__price-hint">
-      <span>inkl. MwSt.</span>
-      <span>€ 37,50 / Liter</span>
-    </div>
-  )
-}
-
-function ProductActions() {
-  return (
-    <div className="product-item__actions">
-      <Button type="primary">Add to cart</Button>
-    </div>
   )
 }
 
