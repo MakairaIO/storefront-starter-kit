46d7c6fffb580398fa3fdbc7ba4178693c0d7ae1
René Mäkeler <maekeler@marmalade.de>
2020-03-22 14:23:20 +0100

    Add Pagination component and pagination functionality #2

--- utils/preparePaginationForQueryString/index.js
+++ utils/preparePaginationForQueryString/index.js
@@ -1,6 +1,4 @@
-export default function preparePaginationForQuerySring(formData) {
-  const count = process.env.PRODUCTS_PER_PAGE
-
+export default function preparePaginationForQuerySring(formData, count) {
   const pageNumber = formData.get('pageNumber') ?? 1
 
   const offset = (pageNumber - 1) * count
