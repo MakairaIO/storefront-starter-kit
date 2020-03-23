1e2d9d13ea1018c67bf99a56998fcf9858429f2b
René Mäkeler <maekeler@marmalade.de>
2020-03-23 15:51:03 +0100

    Add Hero Teaser to example page; Add spacing for all content patterns

--- library/examplePages/Home/index.js
+++ library/examplePages/Home/index.js
@@ -1,11 +1,14 @@
 import Header, { headerVariants } from '../../../patterns/Header'
+import TeaserHero, { teaserHeroVariants } from '../../../patterns/TeaserHero'
 
 const headerProps = headerVariants[0].props
+const teaserHeroProps = teaserHeroVariants[0].props
 
 export default function Home(props) {
   return (
     <>
       <Header {...props} {...headerProps} />
+      <TeaserHero {...props} {...teaserHeroProps} />
     </>
   )
 }
