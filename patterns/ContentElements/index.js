a7c8a1fe662c29261ef458579c6d04396b9d5015
René Mäkeler <maekeler@marmalade.de>
2020-03-27 15:56:49 +0100

    Remove console.logs

--- patterns/ContentElements/index.js
+++ patterns/ContentElements/index.js
@@ -3,7 +3,6 @@ import { TeaserHero, TwoColumnText } from '..'
 export default function ContentElements(props) {
   const { elements = [] } = props
 
-  // console.log(elements.length)
   if (elements.length == 0) return null
 
   // Declare your additional content patterns here
@@ -16,7 +15,6 @@ export default function ContentElements(props) {
     <>
       {elements.map((entry, index) => {
         const Component = components[entry.component]
-        console.log(components)
 
         if (!Component) return null
 
