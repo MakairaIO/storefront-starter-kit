016ae014004d2a6466f7741328a4a40a946d82ae
René Mäkeler <maekeler@marmalade.de>
2020-03-31 11:30:57 +0200

    Add Makaira as default favicon

--- pages/_document.js
+++ pages/_document.js
@@ -40,5 +40,30 @@ export default class MyDocument extends Document {
 }
 
 function Favicon() {
-  return null
+  return (
+    <>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/assets/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/assets/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/assets/favicon/favicon-16x16.png"
+      />
+      <link
+        rel="mask-icon"
+        href="/assets/favicon/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+    </>
+  )
 }
