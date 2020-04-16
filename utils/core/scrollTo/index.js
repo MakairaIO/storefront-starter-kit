7a280e46e693e308e7023d88599d95a60b93bca6
René Mäkeler <maekeler@marmalade.de>
2020-04-16 18:45:40 +0200

    Fix random errors by adding an additional check for window.scrollTo

--- utils/core/scrollTo/index.js
+++ utils/core/scrollTo/index.js
@@ -1,14 +1,20 @@
 export default function scrollTo({ id = '', offset = 0, behavior = 'auto' }) {
-  const element = document.getElementById(id)
+  // Defensive Coding:
+  // Usually, scrollTo should be defined in basically all browser,
+  // but our logs have shown that there are rare cases where scrollTo
+  // is not available in a user's browser
+  if ('scrollTo' in window) {
+    const element = document.getElementById(id)
 
-  if (element) {
-    const elementPos = element.getBoundingClientRect().top
-    const absoluteElementTop = elementPos + window.pageYOffset
-    const middle = absoluteElementTop - window.innerHeight / 2
+    if (element) {
+      const elementPos = element.getBoundingClientRect().top
+      const absoluteElementTop = elementPos + window.pageYOffset
+      const middle = absoluteElementTop - window.innerHeight / 2
 
-    window.scrollTo({
-      top: middle - offset,
-      behavior,
-    })
+      window.scrollTo({
+        top: middle - offset,
+        behavior,
+      })
+    }
   }
 }
