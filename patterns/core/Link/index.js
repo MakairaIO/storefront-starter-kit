a2fb30e5466a1dca87e051ebed6c5087a1aa112d
René Mäkeler <maekeler@marmalade.de>
2020-04-25 16:55:52 +0200

    Add possibility for client-side navigation to internal routes other than the main catch-all route

--- patterns/core/Link/index.js
+++ patterns/core/Link/index.js
@@ -1,11 +1,33 @@
 import { default as NextLink } from 'next/link'
 import { getFullUrl } from '../../../utils'
 
+/**
+ * We have 3 different use-cases for handling links:
+ * 1) A link to another internal route. These are mostly project-specific (like rendering the user's cart)
+ * 2) Links to external pages
+ * 3) The most common: Links to other pages that get served by the main catch-all route
+ */
 export default function Link(props) {
-  const { href, children, ...rest } = props
+  const { href, children, isInternalRoute, ...rest } = props
+
+  /**
+   * Internal Routes
+   */
+  if (isInternalRoute) {
+    const { as } = rest
+
+    return (
+      <NextLink href={href} as={as}>
+        <a {...rest}>{children}</a>
+      </NextLink>
+    )
+  }
 
   const { fullUrl, isExternalLink } = getFullUrl(href)
 
+  /**
+   * External Pages
+   */
   if (isExternalLink) {
     // For information about why to add `rel="noopener noreferrer"`, see: https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/`
     return (
@@ -13,19 +35,22 @@ export default function Link(props) {
         {children}
       </a>
     )
-  } else {
-    const urlInstance = new URL(fullUrl)
-    const pathname = urlInstance.pathname
-    const search = urlInstance.search.replace(/^\?/, '')
-    const hash = urlInstance.hash
-
-    const internalHref = `/frontend/entry?seoUrl=${pathname}&${search}`
-    const externalHref = pathname + (search !== '' ? `?${search}` : '') + hash
-
-    return (
-      <NextLink href={internalHref} as={externalHref}>
-        <a {...rest}>{children}</a>
-      </NextLink>
-    )
   }
+
+  /**
+   * Pages served by catch-all route
+   */
+  const urlInstance = new URL(fullUrl)
+  const pathname = urlInstance.pathname
+  const search = urlInstance.search.replace(/^\?/, '')
+  const hash = urlInstance.hash
+
+  const internalHref = `/frontend/entry?seoUrl=${pathname}&${search}`
+  const externalHref = pathname + (search !== '' ? `?${search}` : '') + hash
+
+  return (
+    <NextLink href={internalHref} as={externalHref}>
+      <a {...rest}>{children}</a>
+    </NextLink>
+  )
 }
