9c58dbd8365848290ab7bf5c0f43bc40472a273c
René Mäkeler <maekeler@marmalade.de>
2020-04-26 13:52:35 +0200

    Fix random errors by adding an additional check for window.scrollTo #2

--- utils/core/scrollTo/index.js
+++ utils/core/scrollTo/index.js
@@ -1,8 +1,10 @@
 export default function scrollTo({ id = '', offset = 0, behavior = 'auto' }) {
-  // Defensive Coding:
-  // Usually, scrollTo should be defined in basically all browser,
-  // but our logs have shown that there are rare cases where scrollTo
-  // is not available in a user's browser
+  /**
+   * Defensive Coding:
+   * Usually, scrollTo should be defined in basically all browser,
+   * but our logs have shown that there are rare cases where scrollTo
+   * is not available in a user's browser
+   */
   if ('scrollTo' in window) {
     const element = document.getElementById(id)
 
@@ -11,10 +13,20 @@ export default function scrollTo({ id = '', offset = 0, behavior = 'auto' }) {
       const absoluteElementTop = elementPos + window.pageYOffset
       const middle = absoluteElementTop - window.innerHeight / 2
 
-      window.scrollTo({
-        top: middle - offset,
-        behavior,
-      })
+      /**
+       * There are some really old browser that do have scrollTo defined on the window object
+       * but refer to an older implementation.
+       * Because of that, we catch occasional errors to avoid crashing the enire application.
+       * We could polyfill with https://github.com/iamdustan/smoothscroll but this edge case is not worth it.
+       */
+      try {
+        window.scrollTo({
+          top: middle - offset,
+          behavior,
+        })
+      } catch (error) {
+        console.error(error)
+      }
     }
   }
 }
