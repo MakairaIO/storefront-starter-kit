9937ec9dfdad6cb9e00c2e90d1e23bcd5c8805c0
René Mäkeler <maekeler@marmalade.de>
2020-05-10 17:53:31 +0200

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -4,20 +4,26 @@ const withCss = require('@zeit/next-css')
 const path = require('path')
 const Dotenv = require('dotenv-webpack')
 
-module.exports = withCss({
-  webpack: (config) => {
-    config.plugins = config.plugins || []
-
-    config.plugins = [
-      ...config.plugins,
-
-      // Read the .env file
-      new Dotenv({
-        path: path.join(__dirname, '.env'),
-        systemvars: true,
-      }),
-    ]
-
-    return config
-  },
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 })
+
+module.exports = withBundleAnalyzer(
+  withCss({
+    webpack: (config) => {
+      config.plugins = config.plugins || []
+
+      config.plugins = [
+        ...config.plugins,
+
+        // Read the .env file
+        new Dotenv({
+          path: path.join(__dirname, '.env'),
+          systemvars: true,
+        }),
+      ]
+
+      return config
+    },
+  })
+)
