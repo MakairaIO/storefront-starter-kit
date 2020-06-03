72cf9941947b5b10d0b364a7bbb589a5c7fabde2
René Mäkeler <maekeler@marmalade.de>
2020-06-03 09:05:54 +0200

    Add Header to example DetailPage

--- library/examplePages/Detail/index.js
+++ library/examplePages/Detail/index.js
@@ -1,12 +1,15 @@
+import Header, { headerVariants } from '../../../patterns/core/Header'
 import ProductDetailInformation, {
   productDetailInformationVariants,
 } from '../../../patterns/core/ProductDetailInformation'
 
+const headerProps = headerVariants[0].props
 const productDetailInformationProps = productDetailInformationVariants[0].props
 
 export default function Home(props) {
   return (
     <>
+      <Header {...props} {...headerProps} />
       <ProductDetailInformation {...props} {...productDetailInformationProps} />
     </>
   )
