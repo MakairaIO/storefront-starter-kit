c477002a040a0ebd8fd52103da5c70b2c2efdd15
René Mäkeler <maekeler@marmalade.de>
2020-06-29 08:04:51 +0200

    Add support for Cloudinary as alternative image provider to S3

--- utils/core/ConfigurationProvider/index.js
+++ utils/core/ConfigurationProvider/index.js
@@ -2,12 +2,32 @@ import { Component, useContext } from 'react'
 
 const ConfigurationContext = React.createContext()
 
+const CLOUDINARY_BASE = 'https://res.cloudinary.com'
+const CLOUDINARY_VERSION = 'v1592420992'
+
+const transformationMapping = {
+  width: 'w',
+  height: 'h',
+  quality: 'q',
+  pixelRatio: 'dpr',
+  format: 'f',
+}
+
 class ConfigurationProvider extends Component {
   static defaultProps = { assetUrl: '' }
 
-  // TODO: Implement height/width/quality handling once we are clear
-  // about the Media-Handling to use
   getImageLink = (options = {}) => {
+    const { source } = options
+
+    if (typeof source == 'string') {
+      return this.getS3Link(options)
+    } else {
+      // objects = cloudinary
+      return this.getCloudinaryLink(options)
+    }
+  }
+
+  getS3Link = (options) => {
     const { source = '' } = options
 
     const { assetUrl } = this.props
@@ -15,6 +35,52 @@ class ConfigurationProvider extends Component {
     return assetUrl + '/' + source
   }
 
+  getCloudinaryLink = (options) => {
+    const { source = {}, transformationString = '', ...rest } = options
+
+    const { cloudName, resourceType, fileName } = source
+
+    const transformations = this.getCloudinaryTransformations(rest)
+
+    // Example: https://res.cloudinary.com/makairafm/image/upload/<transformations>v1592420992/<fileName>
+    return (
+      CLOUDINARY_BASE +
+      '/' +
+      cloudName +
+      '/' +
+      resourceType +
+      '/' +
+      'upload' +
+      '/' +
+      transformations +
+      '/' +
+      transformationString +
+      '/' +
+      CLOUDINARY_VERSION +
+      '/' +
+      fileName
+    )
+  }
+
+  getCloudinaryTransformations = (settings) => {
+    const transformations = Object.entries(settings).reduce(
+      (transformations, currentSetting) => {
+        const [name, value] = currentSetting
+
+        const transformationKey = transformationMapping[name]
+
+        // Make sure to add only known transformations
+        if (transformationKey) {
+          transformations.push(`${transformationKey}_${value}`)
+        }
+
+        return transformations
+      },
+      []
+    )
+
+    return transformations.join(',')
+  }
   render() {
     return (
       <ConfigurationContext.Provider
