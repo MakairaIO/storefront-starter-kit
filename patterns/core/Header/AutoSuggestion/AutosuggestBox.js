527346a4bc7eed04c22027308df2a13a95bf7b43
Thuan Le <thuan.le@8bitrockr.com>
2020-07-17 12:22:02 +0700

    feature/mws-32-auto-suggest: refactor code base on Rene feedback

--- patterns/core/Header/AutoSuggestion/AutosuggestBox.js
+++ patterns/core/Header/AutoSuggestion/AutosuggestBox.js
@@ -1,6 +1,7 @@
 import ProductList from './ProductList'
 import Links from './Links'
-import { Button } from '../../../'
+import { Button, Icon } from '../../../'
+import { useTranslation } from '../../../../utils'
 
 function AutosuggestBox(props) {
   const {
@@ -9,30 +10,41 @@ function AutosuggestBox(props) {
     links = {},
     manufacturer = {},
     closeSearchPopup,
+    goToSearchPage,
   } = props
   const linkList = { category, links, manufacturer }
   const hasLinks = category.count + links.count + manufacturer.count > 0
+  const totalResults =
+    category.total + links.total + manufacturer.total + product.total
+  const { t } = useTranslation()
 
   return (
     <div className="autosuggest-box">
-      <Button
-        variant="icon-only"
-        icon="times"
-        className="autosuggest-box__close"
-        onClick={closeSearchPopup}
-      />
+      <div className="autosuggest-box__wrapper">
+        <Button
+          variant="icon-only"
+          icon="times"
+          className="autosuggest-box__close"
+          onClick={closeSearchPopup}
+        />
 
-      {hasLinks && (
-        <section className="autosuggest-box__links">
-          <Links {...linkList}></Links>
-        </section>
-      )}
+        {hasLinks && (
+          <section className="autosuggest-box__links">
+            <Links {...linkList}></Links>
+          </section>
+        )}
 
-      {product.count > 0 && (
-        <section className="autosuggest-box__products">
-          <ProductList products={product.items} count={product.count} />
-        </section>
-      )}
+        {product.count > 0 && (
+          <section className="autosuggest-box__products">
+            <ProductList products={product.items} count={product.count} />
+          </section>
+        )}
+      </div>
+      <hr />
+      <div className="autosuggest-box__total-result" onClick={goToSearchPage}>
+        {t('FILTER_LABEL_SEE_ALL_RESULTS')(totalResults)}
+        <Icon symbol="chevron-right" />
+      </div>
     </div>
   )
 }
