a7da47304bb53d540d16082d1a65561d5a4d2683
Hao Ho <hao.ho@nfq.asia>
2020-10-14 12:08:17 +0700

    Fixed Advertising banners without link throw exception

--- patterns/core/ConditionalLink/index.js
+++ patterns/core/ConditionalLink/index.js
@@ -3,7 +3,7 @@ import { Link } from '../..'
 export default function ConditionalLink(props) {
   const { href = '', fallbackElement = 'div', children, ...rest } = props
 
-  if (href != '') {
+  if (href && href != '') {
     return (
       <Link href={href} {...rest}>
         {children}
