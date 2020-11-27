4d16790352fdebebfac1ed1e98ec0e45570cde4c
Hao Ho <hao.ho@nfq.asia>
2020-11-27 13:05:23 +0700

    Change product information when choose variant

--- patterns/core/ProductDetailInformation/Buybox.js
+++ patterns/core/ProductDetailInformation/Buybox.js
@@ -2,64 +2,18 @@ import { Dropdown } from '../..'
 import ProductPrices from './ProductPrices'
 import ProductAvailability from './ProductAvailability'
 import ProductActions from './ProductActions'
-import { useState } from 'react'
-
-function prepareVariants({ attributes = [], attributeId = '' }) {
-  const attributeArray = attributeId
-    ? attributes.filter((a) => a.title == attributeId)
-    : attributes
-
-  return attributeArray.map((a) => {
-    return {
-      label: a.value,
-      value: a.value,
-    }
-  })
-}
 
 // TODO: Remove hard-coded implementation
 export default function Buybox(props) {
-  let variants = []
-  const makairaProduct = props['makaira-product']
-  const [productInformation, setProductInformation] = useState(props)
-
-  if (makairaProduct) {
-    variants = Array.isArray(makairaProduct) ? makairaProduct : [makairaProduct]
-  }
-
-  const attributes = variants.map((variant) => {
-    const { attributeStr } = variant
-    return attributeStr
-  })
-
-  // Merge all attributeStr of variants
-  const attributeStr = attributes.reduce((array, attr) => {
-    return array.concat(attr)
-  }, [])
-
-  const attributeStrVariants = prepareVariants({
-    attributes: attributeStr,
-    attributeId: '',
-  })
-
-  const handleOnchange = (attribute) => {
-    const { value } = attribute
-    const variant = variants.find((variant) => {
-      const { attributeStr } = variant
-      return attributeStr.find((a) => a.value === value)
-    })
-
-    setProductInformation(variant)
-  }
-
+  const { chooseVariant = () => {}, variantsAttributeStr = [] } = props
   return (
     <div className="product-detail-information__buybox">
       <div className="product-detail-information__variants">
         <Dropdown
           id="sizeVariant"
           label="Size"
-          options={attributeStrVariants}
-          onChange={(attribute) => handleOnchange(attribute)}
+          options={variantsAttributeStr}
+          onChange={(attribute) => chooseVariant(attribute)}
           className="product-detail-information__variant-select"
         />
       </div>
@@ -68,7 +22,7 @@ export default function Buybox(props) {
         <figure className="product-detail-information__manufacturer"></figure>
 
         <div className="product-detail-information__buxbox-info">
-          <ProductPrices {...productInformation} />
+          <ProductPrices {...props} />
 
           <ProductAvailability {...props} />
         </div>
--- patterns/core/ProductDetailInformation/index.js
+++ patterns/core/ProductDetailInformation/index.js
@@ -2,18 +2,68 @@ import TopHeader from './TopHeader'
 import Image from './Image'
 import Buybox from './Buybox'
 import Description from './Description'
+import { useState } from 'react'
+
+function prepareVariants({ attributes = [], attributeId = '' }) {
+  const attributeArray = attributeId
+    ? attributes.filter((a) => a.title == attributeId)
+    : attributes
+
+  return attributeArray.map((a) => {
+    return {
+      label: a.value,
+      value: a.value,
+    }
+  })
+}
 
 function ProductDetailInformation(props) {
+  const makairaProduct = props['makaira-product']
+  const [productInformation, setProductInformation] = useState(props)
+
+  const hasMultiVariant =
+    Array.isArray(makairaProduct) && makairaProduct.length > 1
+  const variants = hasMultiVariant ? makairaProduct : []
+
+  const attributes = variants.map((variant) => {
+    const { attributeStr } = variant
+    return attributeStr
+  })
+
+  // Merge all attributeStr of variants
+  const attributeStr = attributes.reduce((array, attr) => {
+    return array.concat(attr)
+  }, [])
+
+  const variantsAttributeStr = prepareVariants({
+    attributes: attributeStr,
+    attributeId: '',
+  })
+
+  const chooseVariant = (attribute) => {
+    const { value } = attribute
+    const chosenVariant = variants.find((variant) => {
+      const { attributeStr } = variant
+      return attributeStr.find((a) => a.value === value)
+    })
+
+    setProductInformation(chosenVariant)
+  }
+
   return (
     <section className="product-detail-information">
-      <TopHeader {...props} />
+      <TopHeader {...productInformation} />
 
       <div className="product-detail-information__wrapper">
-        <Image {...props} />
+        <Image {...productInformation} />
 
-        <Buybox {...props} />
+        <Buybox
+          {...productInformation}
+          chooseVariant={chooseVariant}
+          variantsAttributeStr={variantsAttributeStr}
+        />
 
-        <Description {...props} />
+        <Description {...productInformation} />
       </div>
     </section>
   )
