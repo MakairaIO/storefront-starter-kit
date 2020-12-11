b0ab0bf55cf7025570423b7b597e2495aadc2036
René Mäkeler <maekeler@marmalade.de>
2020-12-11 16:22:38 +0100

    Makaira-1294 Fix typo in component name

--- frontend/DetailPage/ProductDetailInformationWithProps.js
+++ frontend/DetailPage/ProductDetailInformationWithProps.js
@@ -1,7 +1,7 @@
 import { ProductDetailInformation } from '../../patterns'
 import { useGlobalData } from '../../utils'
 
-export default function ProductListWithProps() {
+export default function ProductDetailInformationWithProps() {
   const { pageData } = useGlobalData()
 
   const productDetailProps = { ...pageData.data.self }
