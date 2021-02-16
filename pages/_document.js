f2e09b1acecc891b886ae1dc0fe0ea25233d8293
René Mäkeler <maekeler@marmalade.de>
2021-02-16 10:24:34 +0100

    Add gated flag to polyfill link to prevent opera issues when Reflect.construct is polyfilled for IE11

--- pages/_document.js
+++ pages/_document.js
@@ -26,10 +26,12 @@ export default class MyDocument extends Document {
           {/*
            *   WARNING: Do not polyfill Symbol.prototype.description -> it will break Portals in both Safari13 and IE11
            *   -> see: https://github.com/facebook/react/issues/8379#issuecomment-263962787 and https://github.com/facebook/react/issues/8379#issuecomment-418542006
+           *
+           *   WARNING: Do not remove the `gated` flag from the polyfill URL below because it prevents corner cases where native APIs clash with provided polyfills.
 
           <script
             crossOrigin="anonymous"
-            src="https://cdn.polyfill.io/v3/polyfill.min.js?features=Array.from,Array.isArray,Array.prototype.entries,Array.prototype.every,Array.prototype.filter,Array.prototype.find,Array.prototype.forEach,Array.prototype.includes,Array.prototype.indexOf,Array.prototype.keys,Array.prototype.map,Array.prototype.reduce,Array.prototype.some,Array.prototype.values,ArrayBuffer,console,DataView,Date.now,Date.prototype.toISOString,document,Element,Event,fetch,Function.prototype.bind,IntersectionObserver,Intl,JSON,localStorage,Map,Math.trunc,modernizr:es5object,MutationObserver,Number.isInteger,Number.isNaN,Object.assign,Object.entries,Object.getOwnPropertyDescriptors,Object.getOwnPropertySymbols,Object.isExtensible,Object.preventExtensions,Object.setPrototypeOf,Object.values,Promise,Reflect,Reflect.construct,requestAnimationFrame,Set,String.prototype.includes,String.prototype.repeat,String.prototype.startsWith,String.prototype.trim,Symbol,Symbol.iterator,URL,WeakMap,WeakSet,NodeList.prototype.forEach,IntersectionObserver,IntersectionObserverEntry"
+            src="https://cdn.polyfill.io/v3/polyfill.min.js?flags=gated&features=Array.from,Array.isArray,Array.prototype.entries,Array.prototype.every,Array.prototype.filter,Array.prototype.find,Array.prototype.forEach,Array.prototype.includes,Array.prototype.indexOf,Array.prototype.keys,Array.prototype.map,Array.prototype.reduce,Array.prototype.some,Array.prototype.values,ArrayBuffer,console,DataView,Date.now,Date.prototype.toISOString,document,Element,Event,fetch,Function.prototype.bind,IntersectionObserver,Intl,JSON,localStorage,Map,Math.trunc,modernizr:es5object,MutationObserver,Number.isInteger,Number.isNaN,Object.assign,Object.entries,Object.getOwnPropertyDescriptors,Object.getOwnPropertySymbols,Object.isExtensible,Object.preventExtensions,Object.setPrototypeOf,Object.values,Promise,Reflect,Reflect.construct,requestAnimationFrame,Set,String.prototype.includes,String.prototype.repeat,String.prototype.startsWith,String.prototype.trim,Symbol,Symbol.iterator,URL,WeakMap,WeakSet,NodeList.prototype.forEach,IntersectionObserver,IntersectionObserverEntry"
           />
 
            */}
