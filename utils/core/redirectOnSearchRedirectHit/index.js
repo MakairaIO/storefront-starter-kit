49e0c1d6b7e35b82aa4aa240a9b6266bfdd91367
Florian Ludwig <fl@marmalade.group>
2021-02-22 16:53:26 +0000

    Adapt to final structure of search redirects in API

--- utils/core/redirectOnSearchRedirectHit/index.js
+++ utils/core/redirectOnSearchRedirectHit/index.js
@@ -4,7 +4,7 @@ export default function redirectOnSearchRedirectHit({ ctx, searchResult }) {
   const { searchredirect = {} } = searchResult
 
   if (searchredirect.count > 0) {
-    const { targetUrl = '' } = searchredirect.items[0]
+    const { targetUrl = '' } = searchredirect.items[0].fields
 
     if (targetUrl) {
       redirect({ ctx, target: targetUrl, code: 302 })
