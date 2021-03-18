f8825d7c1e3468bc7afca40e8943c3b25cf6a377
René Mäkeler <maekeler@marmalade.de>
2021-03-18 16:36:26 +0100

    Make use of new optional seoTitle field in Landingpage metadata

--- frontend/LandingPage/Metadata.js
+++ frontend/LandingPage/Metadata.js
@@ -5,14 +5,17 @@ export default function Metadata() {
   const { pageData } = useGlobalData()
   const {
     title,
+    seoTitle,
     robotIndex = 'index',
     robotFollow = 'follow',
     ...rest
   } = pageData.data.metadata
 
+  const pageTitle = seoTitle ?? title
+
   return (
     <Head>
-      {title && <title>{title}</title>}
+      {pageTitle && <title>{pageTitle}</title>}
 
       {Object.entries(rest).map(([key, value]) => (
         <meta key={key} name={key} content={value} />
