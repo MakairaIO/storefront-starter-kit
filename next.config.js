9b0a422a635a5dfcccef27f8d4f29a4684252e6a
jan.maegdefrau <jm@marmalade.group>
2021-10-29 14:25:18 +0200

    feat: upgrade versions of next, rc-slider, iframe-resizer and nodejs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 
-const withCss = require('@zeit/next-css')
 const path = require('path')
 const Dotenv = require('dotenv-webpack')
 
@@ -8,24 +7,22 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer(
-  withCss({
-    trailingSlash: true,
+module.exports = withBundleAnalyzer({
+  trailingSlash: true,
 
-    webpack: (config) => {
-      config.plugins = config.plugins || []
+  webpack: (config) => {
+    config.plugins = config.plugins || []
 
-      config.plugins = [
-        ...config.plugins,
+    config.plugins = [
+      ...config.plugins,
 
-        // Read the .env file
-        new Dotenv({
-          path: path.join(__dirname, '.env'),
-          systemvars: true,
-        }),
-      ]
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true,
+      }),
+    ]
 
-      return config
-    },
-  })
-)
+    return config
+  },
+})
