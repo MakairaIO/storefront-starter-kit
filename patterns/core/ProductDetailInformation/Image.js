b132b1de757b6d9f9ec561e5da29089c7babb721
René Mäkeler <rm@marmalade.group>
2022-07-28 14:30:53 +0200

    feat(DX/Performance): add new universal Image component

--- patterns/core/ProductDetailInformation/Image.js
+++ patterns/core/ProductDetailInformation/Image.js
@@ -1,39 +1,21 @@
-import { useConfiguration } from '../../../utils'
-import Head from 'next/head'
+import { Image } from '../..'
 
-export default function Image(props) {
-  const { getImageLink } = useConfiguration()
+export default function ProductImage(props) {
   const { title = '', images = [], activeVariant } = props
 
   const imageSource = activeVariant ? activeVariant.images[0] : images[0]
 
-  const imageLink = getImageLink({
-    source: images[0],
-    format: 'auto',
-    height: 600,
-  })
-
-  const imageLinkRetina = getImageLink({
-    source: imageSource,
-    height: 600,
-    pixelRatio: 2,
-    format: 'auto',
-  })
-
   return (
-    <div className="product-detail-information__image">
-      <Head>
-        <link
-          rel="preload"
-          href={imageLink}
-          as="image"
-          imagesrcset={`${imageLink} 1x, ${imageLinkRetina} 2x`}
-        />
-      </Head>
-
-      <picture>
-        <img srcSet={`${imageLink} 1x, ${imageLinkRetina} 2x`} alt={title} />
-      </picture>
-    </div>
+    <Image
+      alt={title}
+      lazyload={false}
+      preload={true}
+      options={{
+        desktop: {
+          source: imageSource,
+          height: 600,
+        },
+      }}
+    />
   )
 }
