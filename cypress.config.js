19dd8abcc7425906b363933c5bcb9b5cb2475eb9
Nils Fahle <nils.fahle@marlamalde.group>
2022-07-28 14:51:46 +0200

    added seperate .env file for cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,16 @@
 const { defineConfig } = require('cypress')
+const dotenv = require('dotenv')
 
 module.exports = defineConfig({
   video: false,
   e2e: {
     setupNodeEvents() {
       // implement node event listeners here
+
+      const result = dotenv.config({ path: './.env.cypress' })
+      const env = result.error ? {} : result.parsed
+
+      return { env }
     },
     baseUrl: 'http://localhost:5000',
   },
