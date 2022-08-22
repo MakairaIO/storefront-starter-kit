b61e117615a8bd2eba64d012b3b2200a7ff6c6c2
Jan <jm@marmalade.group>
2022-08-22 13:54:06 +0200

    fix(Images): Add fallback to picture_url_main and fix ProductDetailInformation image link for Pali

--- patterns/core/ProductDetailInformation/Image.js
+++ patterns/core/ProductDetailInformation/Image.js
@@ -1,9 +1,14 @@
 import { Image } from '../..'
 
 export default function ProductImage(props) {
-  const { title = '', images = [], activeVariant } = props
+  const { title = '', images = [], activeVariant, picture_url_main } = props
 
-  const imageSource = activeVariant ? activeVariant.images[0] : images[0]
+  let imageSource = activeVariant ? activeVariant.images[0] : images[0]
+
+  // images array may not always be filled, so fallback to picture_url_main if this is the case
+  if (!imageSource) {
+    imageSource = picture_url_main
+  }
 
   return (
     <Image
