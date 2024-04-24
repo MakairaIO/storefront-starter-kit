d60b2a182db1ecd45aec3efb5431f965d2075b05
Sandra Heise <sh@marmalade.group>
2024-04-24 13:41:58 +0200

    MMS-83 cypress discovery image mobile test

--- cypress/e2e/visual/discoveryImage.cy.js
+++ cypress/e2e/visual/discoveryImage.cy.js
@@ -5,4 +5,12 @@ describe('Discovery Image Component', () => {
     cy.wait('@loadImage')
     cy.compareSnapshot('discovery-image')
   })
+
+  it('have a correct style on mobile', () => {
+    cy.viewport(390, 840)
+    cy.intercept('GET', '**/images/discoveryImage/**').as('loadImage')
+    cy.visit(Cypress.env('PALI_PATH') + 'Discovery%20Image_Discovery%20Image')
+    cy.wait('@loadImage')
+    cy.compareSnapshot('discovery-image-mobile')
+  })
 })
