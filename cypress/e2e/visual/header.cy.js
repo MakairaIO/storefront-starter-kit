61cf0328de5b6388ae010ccee1bd37fcd3acda6e
Sandra Heise <sh@marmalade.group>
2024-04-25 08:52:22 +0200

    MMS-83 cypress header tests images

--- cypress/e2e/visual/header.cy.js
+++ cypress/e2e/visual/header.cy.js
@@ -1,18 +1,23 @@
+const retryOptions = {
+  limit: 5, // max number of retries
+  delay: 500, // delay before next iteration, ms
+}
+
 describe('Header Component', () => {
   it('have a correct style', () => {
     cy.visit(Cypress.env('PALI_PATH') + 'Header_Default')
-    cy.compareSnapshot('header')
+    cy.compareSnapshot('header', 0, retryOptions)
   })
 
   it('have a correct style on mobile', () => {
     cy.viewport(390, 840)
     cy.visit(Cypress.env('PALI_PATH') + 'Header_Default')
-    cy.compareSnapshot('header-mobile')
+    cy.compareSnapshot('header-mobile', 0, retryOptions)
   })
 
   it('have a correct style on tablet', () => {
     cy.viewport(1080, 810)
     cy.visit(Cypress.env('PALI_PATH') + 'Header_Default')
-    cy.compareSnapshot('headere-tablet')
+    cy.compareSnapshot('headere-tablet', 0, retryOptions)
   })
 })
