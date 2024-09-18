b364e804c55adeb2e712984a05cdb624055552ad
Phong Nguyen <phongnguyenhh.1996@gmail.com>
2024-09-18 12:55:41 +0700

    adjust eslint

--- next.config.js
+++ next.config.js
@@ -25,4 +25,9 @@ module.exports = withBundleAnalyzer({
 
     return config
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 })
