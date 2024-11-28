3017aeb9ac9dbe41ccf853c3499cd6f2d3a55ae0
Phong Nguyen <phongnguyenhh.1996@gmail.com>
2024-11-28 09:11:37 +0700

    update env

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-const dotenv = require('dotenv')
-dotenv.config()
+require('dotenv').config()
+
 const path = require('path')
-const env =
-  require('dotenv').config({ path: path.join(__dirname, '.env') }).parsed || {}
-const webpack = require('webpack')
-const Dotenv = require('dotenv-webpack')
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.NEXT_PUBLIC_${next}`] = JSON.stringify(env[next])
-  return prev
-}, {})
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
@@ -16,15 +8,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   trailingSlash: true,
-
-  webpack: (config) => {
-    config.plugins = config.plugins || []
-
-    // Add Dotenv plugin to load environment variables
-    config.plugins.push(new webpack.DefinePlugin(envKeys))
-
-    return config
-  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
