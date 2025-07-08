fa7900181faccd8cd8936fc6494b80a759532699
Phong Nguyen <phongnguyenhh.1996@gmail.com>
2025-07-08 14:49:23 +0700

    fix: fix 500 page show randomly

--- utils/core/ConfigurationProvider/index.js
+++ utils/core/ConfigurationProvider/index.js
@@ -52,7 +52,7 @@ class ConfigurationProvider extends Component {
 
     if (typeof source == 'string') {
       // check for shop images
-      if (source.startsWith('https://')) {
+      if (source?.startsWith('https://')) {
         return source
       }
 
@@ -161,7 +161,7 @@ class ConfigurationProvider extends Component {
 
     const transformations = this.getCloudinaryTransformations(rest)
 
-    const versionStr = version
+    const versionStr = version && typeof version.toString === 'function'
       ? version.toString().startsWith('v')
         ? version
         : `v${version}`
